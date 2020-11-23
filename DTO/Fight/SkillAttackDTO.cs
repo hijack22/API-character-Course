9bc035a81493c381b34fd2b2b5c35f486d8793b2
Paul O <porszulak20@gmail.com>
2020-11-23 13:52:32 -0600

    Fight Post API Characters can fight each other

---
+++ DTO/Fight/SkillAttackDTO.cs
@@ -0,0 +1,12 @@
+namespace API_Course.DTO.Fight
+{
+    public class SkillAttackDTo
+    {
+          public int AttackerId {get;set;}
+
+          public int OpponentId {get; set;}
+
+         public int SkillId { get; set; }
+
+    }
+}
--- Services/FightService/IFightService.cs
+++ Services/FightService/IFightService.cs
@@ -7,5 +7,9 @@ namespace API_Course.Services.FightService
     public interface IFightService
     {
           Task<serviceResponse<AttackResultDTo>> WeaponAttack(WeaponAttackDTO request);
+
+          Task<serviceResponse<AttackResultDTo>> skillAttack(SkillAttackDTo request);
+    
+          Task<serviceResponse<FightResultDTO>> Fight(FightRequestDTO request);
     }   
 }
